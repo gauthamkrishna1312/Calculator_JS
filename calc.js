22d7914e13ac10a54c1edc31cccac6ce5c778114
Gautham Krishna P <gauthamkrishna1312@gmail.com>
2023-03-12 21:59:52 +0530

    Calculator0.2 backspace added

--- calc.js
+++ calc.js
@@ -4,6 +4,11 @@ function clickButton(val){
 function clearDisplay(){
     document.getElementById("screen").value = " "
 }
+function clearNum() {
+    var currentText = document.getElementById("screen").value
+    var clearedValue = currentText.substring(0, currentText.length - 1);
+    document.getElementById("screen").value = clearedValue
+}  
 function getResult(){
     var text = document.getElementById("screen").value
     var result = eval(text)
